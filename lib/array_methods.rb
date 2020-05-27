5c72499e543d98a7577ec9761c50cf3734bd31ca
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-27 15:41:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,30 @@
 def using_include(array, element)
-
+@array = array
+@element = element
+@array.include?(@element)
 end
 
 def using_sort(array)
-
+@array = array
+@array.sort
 end
 
 def using_reverse(array)
-
+  @array = array
+  @array.reverse
 end
 
 def using_first(array)
-
+  @array = array
+  @array.first
 end
 
 def using_last(array)
-
+  @array = array
+  @array.last
 end
 
 def using_size(array)
-
+  @array = array
+  @array.size
 end
